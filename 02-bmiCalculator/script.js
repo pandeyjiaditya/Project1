68a0537a346ea27a2ba0ab838ce7a277185e0f4c
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-03-07 19:01:32 +0530

    ranges in BMI

--- 02-bmiCalculator/script.js
+++ 02-bmiCalculator/script.js
@@ -9,6 +9,7 @@ form.addEventListener('submit',function (e) {
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector('#results')
+    const message = document.querySelector('#message')
 
     if (height === '' || height < 0 || isNaN(height)) {
         result.innerHTML = `please enter valid height  ${height}`
@@ -17,7 +18,14 @@ form.addEventListener('submit',function (e) {
     }else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
         result.innerHTML = `<span>${bmi}</span>`
-    }
 
+        if (bmi < 18.6) {
+          message.innerHTML = "Under Weight";
+        } else if (bmi >= 18.6 && bmi <= 24.9) {
+          message.innerHTML = "Normal Range";
+        } else {
+          message.innerHTML = "Over Weight";
+        }
+    }
 
 })
