46a472d893b69741ac05c9668509216f45895e14
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-03-07 14:43:16 +0530

    colour changer completed

--- 01-colourChanger/script.js
+++ 01-colourChanger/script.js
@@ -1,1 +1,14 @@
-document.querySelectorAll(".button")
+const buttons = document.querySelectorAll(".button")
+const body  = document.querySelector('body')
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id) {
+            body.style.backgroundColor = e.target.id
+        }
+    })
+    
+})
