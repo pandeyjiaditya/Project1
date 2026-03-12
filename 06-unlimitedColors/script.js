980e394bba4dcd7e7254e2df2f0c8d59ba0e7097
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-03-12 22:08:35 +0530

    change

---
+++ 06-unlimitedColors/script.js
@@ -0,0 +1,31 @@
+// generate a random color
+
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
+let interval;
+const startChangingColor = function () {
+  if (!interval) {
+    interval = setInterval(changeBGColor, 100);
+  }
+
+  function changeBGColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+
+const stopChangingColor = function () {
+  clearInterval(interval);
+  interval = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
+
+console.log(randomColor());
